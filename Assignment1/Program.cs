77776b6cdb6e4cf1d3c262aa32f5eeda9f6deb5e
Venkat <venkat.p.murthy@gmail.com>
2019-03-20 00:04:16 -0400

    First Assignment

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Assignment1
 {
@@ -7,6 +8,23 @@ namespace Assignment1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int n, i, m = 0, flag = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            n = int.Parse(Console.ReadLine());
+            m = n / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Debug.Write("Number is not Prime.");
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                Debug.Write("Number is Prime.");
+
         }
     }
 }
