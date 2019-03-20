c459b925ddbc715e975a3fad7e6fe4aa26fb6984
Venkat <venkat.p.murthy@gmail.com>
2019-03-20 00:28:18 -0400

    Change 2

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -8,18 +8,17 @@ namespace Assignment1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
             int n1, n2, i, j, m = 0, flag = 0;
             Console.Write("Enter the starting range to check Prime: ");
             n1 = int.Parse(Console.ReadLine());
             Console.Write("Enter the ending range to check Prime: ");
             n2 = int.Parse(Console.ReadLine());
 
-            for (i=n1; i<= n2; i++)
+            for (i = n1; i <= n2; i++)
             {
                 flag = 0;
                 m = i / 2;
-                for (j=2; j <= m; j++)
+                for (j = 2; j <= m; j++)
                 {
                     if (i % j == 0)
                     {
@@ -29,9 +28,11 @@ namespace Assignment1
                     }
                 }
                 if (flag == 0)
-                 Debug.Write(i + " is Prime.");
+                    Debug.Write(i + " is Prime.");
             }
-            
+
+            Console.Write("Enter to exit console");
         }
     }
+
 }
